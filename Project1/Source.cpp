36ad0ce86881a33e936f8f5e30f696885ab1d046
Torin Sparks <tsparks9038@conestogac.on.ca>
2022-09-16 12:40:41 -0400

    Adds names to vector space.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,3 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct STUDENT_DATA {
+	string firstName;
+	string lastName;
+};
+
 int main() {
+	ifstream fin;
+	string line;
+	vector<STUDENT_DATA> studentData;
+
+	fin.open("StudentData.txt");
+
+	if (fin.is_open()) {
+		while (!fin.eof()) {
+			string line, first, last;
+			STUDENT_DATA student;
+
+			getline(fin, last, ',');
+			getline(fin, first);
+
+			student.firstName = first;
+			student.lastName = last;
+
+			studentData.push_back(student);
+		}
+
+		fin.close();
+	}
+
+	else {
+		cout << "Cannot open file";
+	}
+
 	return 1;
 }
