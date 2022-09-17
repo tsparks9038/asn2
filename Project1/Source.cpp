fbab0e7820a2aaaea4c56a4e89277657a5b187e0
Torin Sparks <tsparks9038@conestogac.on.ca>
2022-09-17 16:47:27 -0400

    pre release

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -7,13 +7,16 @@ using namespace std;
 
 #ifdef _DEBUG
 #define LOG(x) cout << x << endl;
+#define MODE cout << "Program running in pre-release" << endl;
 #else
 #define LOG(x)
+#define MODE cout << "Program running in standard" << endl;
 #endif
 
 struct STUDENT_DATA {
 	string firstName;
 	string lastName;
+	string email;
 };
 
 int main() {
@@ -21,23 +24,28 @@ int main() {
 	string line;
 	vector<STUDENT_DATA> studentData;
 
-	fin.open("StudentData.txt");
+	MODE;
+
+	fin.open("StudentData_Emails.txt");
 
 	if (fin.is_open()) {
 		while (!fin.eof()) {
-			string line, first, last;
+			string line, first, last, email;
 			STUDENT_DATA student;
 
 			getline(fin, last, ',');
-			getline(fin, first);
+			getline(fin, first, ',');
+			getline(fin, email);
 
 			student.firstName = first;
 			student.lastName = last;
+			student.email = email;
 
 			studentData.push_back(student);
 
 			LOG(student.firstName);
 			LOG(student.lastName);
+			LOG(student.email);
 		}
 
 		fin.close();
